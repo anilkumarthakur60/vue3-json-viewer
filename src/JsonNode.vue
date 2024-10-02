462a2bf533917ddf6efe10863cf5393542c41253
Er. Anil Kumar Thakur <anilkumarthakur60@gmail.com>
2024-10-02 08:34:09 +0545

    fix: minor changes

--- src/JsonNode.vue
+++ src/JsonNode.vue
@@ -1,10 +1,44 @@
 <template>
-  <div :style="{ marginLeft: level * 10 + 'px', padding: '2px 0' }" :class="{ 'json-viewer-dark': darkMode && level === 0, 'json-viewer-light': !darkMode && level === 0 }">
+  <div
+    :style="{ marginLeft: level * 10 + 'px', padding: '2px 0' }"
+    :class="{
+      'json-viewer-dark': darkMode && level === 0,
+      'json-viewer-light': !darkMode && level === 0,
+    }"
+  >
     <div v-if="isObject">
-      <span @click="toggle" v-if="level!==0"  :class="['toggle-btn', darkMode ? 'toggle-btn-dark' : 'toggle-btn-light']">{{ expanded ? '-' : '+' }}</span>
-      <span class="type-label" @click="toggle" :style="{ color: getBracketColor(level) }">{{ parentKey }} {</span>
-      <DocumentDuplicateIcon @click="copyNode" :class="['copy-icon', darkMode ? 'copy-icon-dark' : 'copy-icon-light']" />
-      <div v-show="expanded" :class="['node-children', darkMode ? 'node-children-dark' : 'node-children-light']">
+      <span
+        @click="toggle"
+        v-if="level !== 0"
+        :class="[
+          'toggle-btn',
+          darkMode ? 'toggle-btn-dark' : 'toggle-btn-light',
+        ]"
+        >{{ expanded ? "-" : "+" }}</span
+      >
+      <span
+        class="type-label"
+        @click="toggle"
+        :style="{ color: getBracketColor(level) }"
+        >{{ parentKey }}: {</span
+      >
+      <DocumentDuplicateIcon
+        @click="copyNode"
+        :class="['copy-icon', darkMode ? 'copy-icon-dark' : 'copy-icon-light']"
+      />
+      <span
+        class="key-count"
+        @click="toggle"
+        :style="{ color: getBracketColor(level) }"
+        >({{ Object.keys(props.data).length }}...)</span
+      >
+      <div
+        v-show="expanded"
+        :class="[
+          'node-children',
+          darkMode ? 'node-children-dark' : 'node-children-light',
+        ]"
+      >
         <JsonNode
           v-for="(value, key) in props.data"
           :key="key"
@@ -14,13 +48,50 @@
           :darkMode="darkMode"
         />
       </div>
-      <span class="type-label" @click="toggle" :style="{ color: getBracketColor(level), marginLeft: level * 12 + 'px' }">},</span>
+      <span
+        class="type-label"
+        @click="toggle"
+        :style="{
+          color: getBracketColor(level),
+          marginLeft: level * 12 + 'px',
+        }"
+        >},</span
+      >
     </div>
     <div v-else-if="isArray">
-      <span @click="toggle" v-if="level!==0" :class="['toggle-btn', darkMode ? 'toggle-btn-dark' : 'toggle-btn-light']">{{ expanded ? '-' : '+' }}</span>
-      <span class="type-label" @click="toggle" :style="{ color: getBracketColor(level) }">{{ parentKey }} []</span>
-      <DocumentDuplicateIcon @click="copyNode" :class="['copy-icon', darkMode ? 'copy-icon-dark' : 'copy-icon-light']" />
-      <div v-show="expanded" :class="['node-children', darkMode ? 'node-children-dark' : 'node-children-light']">
+      <span
+        @click="toggle"
+        v-if="level !== 0"
+        :class="[
+          'toggle-btn',
+          darkMode ? 'toggle-btn-dark' : 'toggle-btn-light',
+        ]"
+        >{{ expanded ? "-" : "+" }}</span
+      >
+      <span
+        class="type-label"
+        @click="toggle"
+        :style="{ color: getBracketColor(level) }"
+        >{{ parentKey }} [
+        <span
+          class="key-count"
+          @click="toggle"
+          :style="{ color: getBracketColor(level) }"
+          >({{ Object.keys(props.data).length }}...)</span
+        >
+        ]</span
+      >
+      <DocumentDuplicateIcon
+        @click="copyNode"
+        :class="['copy-icon', darkMode ? 'copy-icon-dark' : 'copy-icon-light']"
+      />
+      <div
+        v-show="expanded"
+        :class="[
+          'node-children',
+          darkMode ? 'node-children-dark' : 'node-children-light',
+        ]"
+      >
         <JsonNode
           v-for="(item, index) in props.data"
           :key="index"
@@ -31,17 +102,27 @@
         />
       </div>
     </div>
-    <div v-else :class="['leaf-node', darkMode ? 'leaf-node-dark' : 'leaf-node-light']">
-      <span class="key" @click="toggle">{{ parentKey }}</span>:
-      <span :class="[valueClass, darkMode ? 'value-dark' : 'value-light']"> {{ displayDataType }} {{ displayValue }}</span>
-      <DocumentDuplicateIcon v-if="isCopyable" @click="copyNode" :class="['copy-icon', darkMode ? 'copy-icon-dark' : 'copy-icon-light']" />
+    <div
+      v-else
+      :class="['leaf-node', darkMode ? 'leaf-node-dark' : 'leaf-node-light']"
+    >
+      <span class="key" @click="toggle">{{ parentKey }}</span
+      >:
+      <span :class="[valueClass, darkMode ? 'value-dark' : 'value-light']">
+        {{ displayDataType }} {{ displayValue }}</span
+      >
+      <DocumentDuplicateIcon
+        v-if="isCopyable"
+        @click="copyNode"
+        :class="['copy-icon', darkMode ? 'copy-icon-dark' : 'copy-icon-light']"
+      />
     </div>
   </div>
 </template>
 <script setup>
-import { ref, computed, onMounted } from 'vue';
-import { defineProps } from 'vue';
-import { DocumentDuplicateIcon } from "@heroicons/vue/24/solid"
+import { ref, computed, onMounted } from "vue";
+import { defineProps } from "vue";
+import { DocumentDuplicateIcon } from "@heroicons/vue/24/solid";
 const props = defineProps({
   data: {
     type: [Object, Array, String, Number, Boolean, Function, Date, RegExp],
@@ -53,7 +134,7 @@ const props = defineProps({
   },
   parentKey: {
     type: [String, Number],
-    default: '',
+    default: "",
   },
   darkMode: {
     type: Boolean,
@@ -69,36 +150,45 @@ onMounted(() => {
   }
 });
 
-const isObject = computed(() => typeof props.data === 'object' && !Array.isArray(props.data) && props.data !== null);
+const isObject = computed(
+  () =>
+    typeof props.data === "object" &&
+    !Array.isArray(props.data) &&
+    props.data !== null,
+);
 const isArray = computed(() => Array.isArray(props.data));
-const isCopyable = computed(() => ['string', 'number', 'boolean', 'object'].includes(typeof props.data) && props.data !== null);
+const isCopyable = computed(
+  () =>
+    ["string", "number", "boolean", "object"].includes(typeof props.data) &&
+    props.data !== null,
+);
 const displayValue = computed(() => {
-  if (typeof props.data === 'string') return `"${props.data}"`;
+  if (typeof props.data === "string") return `"${props.data}"`;
   if (props.data instanceof Date) return props.data.toISOString();
   if (props.data instanceof RegExp) return props.data.toString();
   return props.data;
 });
 const valueClass = computed(() => {
-  if (typeof props.data === 'string') return 'string-value';
-  if (typeof props.data === 'number') return 'number-value';
-  if (typeof props.data === 'boolean') return 'boolean-value';
-  if (props.data === null) return 'null-value';
-  if (props.data instanceof Date) return 'date-value';
-  if (props.data instanceof RegExp) return 'regexp-value';
-  return '';
+  if (typeof props.data === "string") return "string-value";
+  if (typeof props.data === "number") return "number-value";
+  if (typeof props.data === "boolean") return "boolean-value";
+  if (props.data === null) return "null-value";
+  if (props.data instanceof Date) return "date-value";
+  if (props.data instanceof RegExp) return "regexp-value";
+  return "";
 });
 const displayDataType = computed(() => {
-  if (typeof props.data === 'string') return 'string';
-  if (typeof props.data === 'number') return 'number';
-  if (typeof props.data === 'boolean') return 'boolean';
-  if (props.data === null) return 'null';
-  if (props.data instanceof Date) return 'date';
-  if (props.data instanceof RegExp) return 'regexp';
-  if (Array.isArray(props.data)) return 'array';
-  if (typeof props.data === 'function') return 'function';
-  if (typeof props.data === 'undefined') return 'undefined';
-  if (typeof props.data === 'object') return 'object';
-  return 'unknown';
+  if (typeof props.data === "string") return "string";
+  if (typeof props.data === "number") return "number";
+  if (typeof props.data === "boolean") return "boolean";
+  if (props.data === null) return "null";
+  if (props.data instanceof Date) return "date";
+  if (props.data instanceof RegExp) return "regexp";
+  if (Array.isArray(props.data)) return "array";
+  if (typeof props.data === "function") return "function";
+  if (typeof props.data === "undefined") return "undefined";
+  if (typeof props.data === "object") return "object";
+  return "unknown";
 });
 
 function toggle() {
@@ -107,11 +197,13 @@ function toggle() {
 
 function copyNode() {
   const nodeData = JSON.stringify(props.data, null, 2);
-  navigator.clipboard.writeText(nodeData)
+  navigator.clipboard.writeText(nodeData);
 }
 
 function getBracketColor(level) {
-  const colors = props.darkMode ? ['#e06c75', '#e5c07b', '#98c379', '#56b6c2', '#61afef', '#c678dd'] : ['#d32f2f', '#fbc02d', '#388e3c', '#0288d1', '#1976d2', '#7b1fa2'];
+  const colors = props.darkMode
+    ? ["#e06c75", "#e5c07b", "#98c379", "#56b6c2", "#61afef", "#c678dd"]
+    : ["#d32f2f", "#fbc02d", "#388e3c", "#0288d1", "#1976d2", "#7b1fa2"];
   return colors[level % colors.length];
 }
 </script>
