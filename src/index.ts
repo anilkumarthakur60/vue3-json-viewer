f73661e7caf0de5df049f1fd91b40d0fb08fc355
Er. Anil Kumar Thakur <anilkumarthakur60@gmail.com>
2024-10-03 19:41:55 +0545

    fix: minor changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { App } from 'vue';
 import JsonViewer from './JsonViewer.vue';
 import { JsonViewerProps } from './types/jsonViewerTypes';
 import { useJsonViewer } from './hooks/useJsonViewer';
-
+import './global.scss';
 const jsonViewerPlugin = {
   install(app: App) {
     app.component('JsonViewer', JsonViewer);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx(), VueDevTools(), dts()],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@anilkumarthakur/vue3-json-viewer',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+    cssCodeSplit: true,
+  },
+  // css: {
+  //     preprocessorOptions: {
+  //         scss: {
+  //             additionalData: `@import "./src/assets/scss/global.scss";`
+  //         }
+  //     }
+  // },
+
+  server: {
+    host: '0.0.0.0', // Allows access from the local network
+    port: 3000, // You can change this to any port number you prefer
+  },
 });
