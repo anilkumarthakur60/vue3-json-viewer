e576f2cc40381e089637ca0b056d605d5eac7e1c
Er. Anil Kumar Thakur <anilkumarthakur60@gmail.com>
2024-10-03 19:45:27 +0545

    fix: minor changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -17,17 +17,14 @@ module.exports = {
     {
       files: [
         'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
+        'cypress/support/**/*.{js,ts,jsx,tsx}',
       ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
+      extends: ['plugin:cypress/recommended'],
+    },
   ],
   rules: {
     // 'no-console': 'error',
     'no-console': 'off',
     'vue/multi-word-component-names': 'off',
-
   },
-}
+};
--- vite.config.js
+++ vite.config.js
@@ -6,35 +6,37 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import VueDevTools from 'vite-plugin-vue-devtools';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueJsx(), VueDevTools(), dts()],
-    build: {
-        copyPublicDir: false,
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: '@anilkumarthakur/vue3-json-viewer',
-            fileName: function (format) { return "index.".concat(format, ".js"); },
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: {
-                    vue: 'Vue',
-                },
-            },
-        },
-        sourcemap: true,
-        emptyOutDir: true,
-        cssCodeSplit: true,
+  plugins: [vue(), vueJsx(), VueDevTools(), dts()],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@anilkumarthakur/vue3-json-viewer',
+      fileName: function (format) {
+        return 'index.'.concat(format, '.js');
+      },
     },
-    // css: {
-    //     preprocessorOptions: {
-    //         scss: {
-    //             additionalData: `@import "./src/assets/scss/global.scss";`
-    //         }
-    //     }
-    // },
-    server: {
-        host: '0.0.0.0', // Allows access from the local network
-        port: 3000, // You can change this to any port number you prefer
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
     },
+    sourcemap: true,
+    emptyOutDir: true,
+    cssCodeSplit: true,
+  },
+  // css: {
+  //     preprocessorOptions: {
+  //         scss: {
+  //             additionalData: `@import "./src/assets/scss/global.scss";`
+  //         }
+  //     }
+  // },
+  server: {
+    host: '0.0.0.0', // Allows access from the local network
+    port: 3000, // You can change this to any port number you prefer
+  },
 });
