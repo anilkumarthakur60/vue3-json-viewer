846ca58506dcd6387c2f0adec333e4c4c1c1ed2f
Er. Anil Kumar Thakur <anilkumarthakur60@gmail.com>
2024-10-23 15:09:08 +0545

    fix: minor changes

--- src/hooks/useJsonViewer.ts
+++ src/hooks/useJsonViewer.ts
@@ -1,5 +1,5 @@
 import { ref, computed } from 'vue';
-import { JsonViewerProps } from '../package/types/jsonViewerTypes';
+import { JsonViewerProps } from '../package';
 
 export default function useJsonViewer(props: JsonViewerProps) {
   const expanded = ref(false);
